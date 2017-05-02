b950eb5c85fb8fe682c8794974f44569cd631b6c
aschaible <adam.schaible@gmail.com>
2017-05-02 14:38:41 -0400

    Initial import

---
+++ log4net.Filter.FuzzyStringFilter.Tests/FuzzyStringFilterTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using log4net.Core;
+using NUnit.Framework;
+
+namespace log4net.Filter.FuzzyStringFilter.Tests
+{
+    [TestFixture]
+    public class FuzzyStringFilterTest
+    {
+        [Test]
+        public void TestFilterDeny()
+        {
+            var fuzzyStringFilter = new FuzzyStringFilter();
+
+            var logEventOne = GetLoggingEvent("This is a test message", DateTimeOffset.Now);
+            var logEventTwo = GetLoggingEvent("This is a test message2", DateTimeOffset.Now);
+            var logEventThree = GetLoggingEvent("This is a totally different message", DateTimeOffset.Now);
+            var logEventFour = GetLoggingEvent("This is a totally different message too", DateTimeOffset.Now);
+            var logEventFive = GetLoggingEvent("This is a test message", DateTimeOffset.Now.AddSeconds(301));
+
+            var decisionOne = fuzzyStringFilter.Decide(logEventOne);
+            var decisionTwo = fuzzyStringFilter.Decide(logEventTwo);
+            var decisionThree = fuzzyStringFilter.Decide(logEventThree);
+            var decisionFour = fuzzyStringFilter.Decide(logEventFour);
+            var decisionFive = fuzzyStringFilter.Decide(logEventFive);
+
+            Assert.AreEqual(FilterDecision.Neutral, decisionOne);
+            Assert.AreEqual(FilterDecision.Deny, decisionTwo);
+            Assert.AreEqual(FilterDecision.Neutral, decisionThree);
+            Assert.AreEqual(FilterDecision.Deny, decisionFour);
+            Assert.AreEqual(FilterDecision.Neutral, decisionFive);
+        }
+
+        private LoggingEvent GetLoggingEvent(string text, DateTimeOffset timestamp)
+        {
+            return new LoggingEvent(new LoggingEventData
+            {
+                TimeStampUtc = timestamp.UtcDateTime,
+                Message = text
+            });
+        }
+    }
+}
---
+++ log4net.Filter.FuzzyStringFilter/FuzzyStringFilter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DuoVia.FuzzyStrings;
+using log4net.Core;
+
+namespace log4net.Filter.FuzzyStringFilter
+{
+    public class FuzzyStringFilter : FilterSkeleton
+    {
+        private readonly List<AcceptedMessage> _acceptedMessages;
+
+        public FuzzyStringFilter()
+        {
+            _acceptedMessages = new List<AcceptedMessage>();
+            DecaySeconds = 300;
+            MinimumDiceCoefficient = .8;
+        }
+
+        public override FilterDecision Decide(LoggingEvent loggingEvent)
+        {
+            var decision = FilterDecision.Neutral;
+            var matchedInList = false;
+            var renderedMessage = loggingEvent.RenderedMessage;
+            
+            if (_acceptedMessages.Any())
+            {
+                foreach (var acceptedMessage in _acceptedMessages)
+                {
+                    var matchCoefficient = acceptedMessage.MessageText.DiceCoefficient(renderedMessage);
+                    if (matchCoefficient >= MinimumDiceCoefficient)
+                    {
+                        //If the decay period has expired, do not deny the message
+                        if (acceptedMessage.MessageTimestamp.AddSeconds(DecaySeconds).UtcDateTime < loggingEvent.TimeStampUtc)
+                        {
+                            acceptedMessage.MessageTimestamp = DateTimeOffset.Now;
+                            acceptedMessage.MessageText = renderedMessage;
+                        }
+                        else
+                        {
+                            decision = FilterDecision.Deny;
+                        }
+                        matchedInList = true;
+                        break;
+                    }
+                }
+            }
+
+            //If it's not getting filtered out or updated, add it to the list
+            if (!matchedInList)
+            {
+                _acceptedMessages.Add(new AcceptedMessage
+                {
+                    MessageText = loggingEvent.RenderedMessage,
+                    MessageTimestamp = DateTimeOffset.Now
+                });
+            }
+
+            return decision;
+        }
+
+        private class AcceptedMessage
+        {
+            public DateTimeOffset MessageTimestamp { get; set; }
+            public string MessageText { get; set; }
+        }
+
+        public int DecaySeconds { get; set; }
+
+        public double MinimumDiceCoefficient { get; set; }
+    }
+}
